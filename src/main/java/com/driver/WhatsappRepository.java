8d1dd5789fc0f37a192d97923be38909463b3184
Tushar Dey <tushar24dey@gmail.com>
2024-01-28 16:50:06 +0530

    done

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -14,6 +14,8 @@ public class WhatsappRepository {
     private HashMap<Message, User> senderMap;
     private HashMap<Group, User> adminMap;
     private HashSet<String> userMobile;
+    private HashMap<String,User> userDb=new HashMap<>();
+    private HashMap<Integer,Message> messageDb=new HashMap<>();
     private int customGroupCount;
     private int messageId;
 
@@ -25,5 +27,121 @@ public class WhatsappRepository {
         this.userMobile = new HashSet<>();
         this.customGroupCount = 0;
         this.messageId = 0;
+    }
+    public boolean checkMobileNumberExist(String mobile){
+        return userMobile.contains(mobile);
+    }
+    public void saveUser(String name,String mobile){
+        userDb.put(mobile,new User(name,mobile));
+        userMobile.add(mobile);
+    }
+    public void updateCount(){
+        customGroupCount++;
+    }
+    public int getCount(){
+        return customGroupCount;
+    }
+    public void addAdmin(Group group,User user){
+        adminMap.put(group,user);
+    }
+    public void addGroup(Group group,List<User> users){
+        groupUserMap.put(group,users);
+    }
+    public void updateId(){
+        messageId++;
+    }
+    public int getMessageId(){
+        return messageId;
+    }
+
+    public void addMessage(Message message) {
+        messageDb.put(message.getId(),message);
+    }
+    public boolean groupExistInAdminDb(Group group){
+        return adminMap.containsKey(group);
+    }
+    public List<User> getUserList(Group group){
+        return groupUserMap.get(group);
+    }
+    public void addMessageUser(Message message,User sender){
+        senderMap.put(message,sender);
+    }
+
+    public void addGroupMessage(Group group, Message message) {
+        if(!groupMessageMap.containsKey(group)){
+            groupMessageMap.put(group,new ArrayList<>());
+        }
+        groupMessageMap.get(group).add(message);
+    }
+
+    public int getGroupMessageCount(Group group) {
+        return groupMessageMap.get(group).size();
+    }
+    public User getAdmin(Group group){
+        return adminMap.get(group);
+    }
+
+    public Group getGroupByUser(User user) {
+        for(Group group:groupUserMap.keySet()){
+            List<User> users=groupUserMap.get(group);
+            for(User u:users){
+                if(u.getName().equals(user.getName()) && u.getMobile().equals(user.getMobile())){
+                    return group;
+                }
+            }
+        }
+        return null;
+    }
+
+    public int removeUser(Group group, User user) {
+        List<User> users=groupUserMap.get(group);
+        int numberOfUsers=0;
+        int numberOfMessages=0;
+        int overallMessages=0;
+
+        if(users.size()==2){
+            groupUserMap.remove(group);
+            List<Message> messages=groupMessageMap.get(group);
+            for(Message message:messages){
+                senderMap.remove(message);
+            }
+            groupMessageMap.remove(group);
+        }else{
+            users.remove(user);
+            groupUserMap.put(group,users);
+            numberOfUsers=users.size();
+
+            List<Message> messages=groupMessageMap.get(group);
+            for(Message message:messages){
+                User user1=senderMap.get(message);
+                if(user1.getName().equals(user.getName()) && user1.getMobile().equals(user.getMobile())) {
+                    senderMap.remove(message);
+                    messages.remove(message);
+                }
+            }
+            groupMessageMap.put(group,messages);
+            numberOfMessages=messages.size();
+
+
+        }
+        for(Group group1:groupMessageMap.keySet()){
+            overallMessages+=groupMessageMap.get(group1).size();
+        }
+        return numberOfUsers+numberOfMessages+overallMessages;
+    }
+
+    public List<Message> findMessage(Date start, Date end, int k)  {
+        List<Message> messagesInBetween=new ArrayList<>();
+        for(Group group:groupMessageMap.keySet()){
+            List<Message> messages=groupMessageMap.get(group);
+            for(Message message:messages){
+                if(message.getTimestamp().compareTo(start)>0 && message.getTimestamp().compareTo(end)<0){
+                    if(message.getId()>=k){
+                        messagesInBetween.add(message);
+                    }
+                }
+            }
+        }
+        return messagesInBetween;
     }
 }
---
+++ src/main/java/com/driver/WhatsappService.java
@@ -0,0 +1,117 @@
+package com.driver;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class WhatsappService {
+    WhatsappRepository whatsappRepository=new WhatsappRepository();
+    public String createUser(String name, String mobile) throws Exception {
+        boolean check=whatsappRepository.checkMobileNumberExist(mobile);
+        if(!check){
+            whatsappRepository.saveUser(name,mobile);
+        }else {
+            throw new Exception("User already exists");
+        }
+        return "SUCCESS";
+    }
+
+    public Group createGroup(List<User> users) {
+        Group group;
+        User user1=users.get(0);
+        User user2=users.get(1);
+        if(users.size() == 2){
+            group=new Group(user2.getName(),users.size());
+        }else{
+            whatsappRepository.updateCount();
+            int count= whatsappRepository.getCount();
+            group=new Group("Group "+count,users.size());
+        }
+        whatsappRepository.addAdmin(group,user1);
+        whatsappRepository.addGroup(group,users);
+        return group;
+    }
+
+    public int createMessage(String content) {
+        whatsappRepository.updateId();
+        int id= whatsappRepository.getMessageId();
+        Message message=new Message(id,content,new Date());
+        whatsappRepository.addMessage(message);
+        return id;
+    }
+
+    public int sendMessage(Message message, User sender, Group group) throws Exception {
+        if(!whatsappRepository.groupExistInAdminDb(group)){
+            throw new Exception("Group does not exist");
+        }
+        List<User> users=whatsappRepository.getUserList(group);
+        boolean flag=false;
+        for(User user:users){
+            if(user.getName().equals(sender.getName()) && user.getMobile().equals(sender.getMobile())){
+                flag=true;
+                break;
+            }
+        }
+        if(!flag){
+            throw new Exception("You are not allowed to send message");
+        }
+        whatsappRepository.addMessageUser(message,sender);
+        whatsappRepository.addGroupMessage(group,message);
+
+        return whatsappRepository.getGroupMessageCount(group);
+    }
+
+    public String changeAdmin(User approver, User user, Group group) throws Exception {
+        if(!whatsappRepository.groupExistInAdminDb(group)){
+            throw new Exception("Group does not exist");
+        }
+        User user1=whatsappRepository.getAdmin(group);
+        if(!user1.getName().equals(approver.getName()) || user1.getMobile().equals(approver.getMobile())){
+            throw new Exception("Approver does not have rights");
+        }
+
+        List<User> users=whatsappRepository.getUserList(group);
+        boolean flag=false;
+        for(User u:users){
+            if(u.getName().equals(user.getName()) && u.getMobile().equals(user.getMobile())){
+                flag=true;
+                break;
+            }
+        }
+        if(!flag){
+            throw new Exception("User is not a participant");
+        }
+        whatsappRepository.addAdmin(group,user);
+        return "SUCCESS";
+    }
+
+    public int removeUser(User user) throws Exception {
+        Group group=whatsappRepository.getGroupByUser(user);
+        if (group==null){
+            throw new Exception("User not found");
+        }
+        User admin=whatsappRepository.getAdmin(group);
+        if(admin.getMobile().equals(user.getMobile()) && admin.getName().equals(user.getName())){
+            throw new Exception("Cannot remove admin");
+        }
+        return whatsappRepository.removeUser(group,user);
+    }
+
+    public String findMessage(Date start, Date end, int k) throws Exception{
+        List<Message> messages= whatsappRepository.findMessage(start,end,k);
+        if(messages.isEmpty()){
+            throw new Exception("K is greater than the number of messages");
+        }
+        int maxMessage=Integer.MIN_VALUE;
+        Message message=null;
+        for(Message message1:messages){
+            if(message1.getId()>maxMessage){
+                maxMessage=message1.getId();
+                message=message1;
+            }
+        }
+        return message==null?"null":message.getContent();
+    }
+}
